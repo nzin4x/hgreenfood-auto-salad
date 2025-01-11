79295313d05d308d2acaacb339c2dd04197fe31c
nz <nzin4x@gmail.com>
2025-01-11 17:46:54 +0900

    holiday 조회 구조화

--- holiday.py
+++ holiday.py
@@ -2,37 +2,49 @@ import requests
 
 from util import load_yaml
 
-# API URL
-url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo'
+class Holiday:
+    def __init__(self, config):
+        self.config = config
 
+    def get_holidays(self, year: int, month: int):
+        # 요청할 파라미터 설정
+        params = {
+            'serviceKey': config['data.go.kr']['api']['key'],
+            'pageNo': '1',
+            'numOfRows': '10',
+            'solYear': str(year),
+            'solMonth': str(month).zfill(2)
+        }
+
+        # GET 요청
+        response = requests.get(config['data.go.kr']['api']['holiday']['endpoint'], params=params)
+
+        import xml.etree.ElementTree as ET
+
+        # 주어진 XML 데이터
+        xml_data = response.content
+
+        # XML 파싱
+        root = ET.fromstring(xml_data)
+
+        # locdate 값을 저장할 리스트
+        locdates = []
+
+        # locdate 요소 추출
+        for item in root.findall('.//item'):
+            locdate = item.find('locdate').text
+            locdates.append(locdate)
+
+        return locdates
+
+
+# API URL
 config = load_yaml('config.user.yaml')
 
-# 요청할 파라미터 설정
-params = {
-    'serviceKey': config['data.go.kr-apikey'],
-    'pageNo': '1',
-    'numOfRows': '10',
-    'solYear': '2025',
-    'solMonth': '01'
-}
+if __name__ == '__main__':
+    holidays = Holiday(config).get_holidays(2025,10)
+    print(holidays)
 
-# GET 요청
-response = requests.get(url, params=params)
 
-import xml.etree.ElementTree as ET
 
-# 주어진 XML 데이터
-xml_data = response.content
 
-# XML 파싱
-root = ET.fromstring(xml_data)
-
-# locdate 값을 저장할 리스트
-locdates = []
-
-# locdate 요소 추출
-for item in root.findall('.//item'):
-    locdate = item.find('locdate').text
-    locdates.append(locdate)
-
-print(locdates)
---
+++ test/holiday_test.py
@@ -0,0 +1,5 @@
+import sample
+
+
+def test_initdb():
+    sample.init_db()
