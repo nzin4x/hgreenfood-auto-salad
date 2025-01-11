7b6ba2f2b97500581bf376441f2110c423b96246
nz <nzin4x@gmail.com>
2025-01-11 17:55:28 +0900

    holiday 단위 테스트

--- test/holiday_test.py
+++ test/holiday_test.py
@@ -1,5 +1,15 @@
-import sample
+import pytest
+
+from holiday import Holiday
+from util import load_yaml
 
 
-def test_initdb():
-    sample.init_db()
+@pytest.fixture
+def userconfig():
+    yield  load_yaml('config.user.yaml')
+
+def test_holiday_api(userconfig):
+    holidays = Holiday(userconfig).get_holidays(2025, 1)
+    assert '20250127' in holidays
+    holidays = Holiday(userconfig).get_holidays(2025, 10)
+    assert '20251005' in holidays
